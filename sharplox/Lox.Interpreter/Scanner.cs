4cfe276625bc4745273d2c10a4bb0fce581c9586
Alexandru Doru Grosu <alexandru.d.grosu@gmail.com>
2022-04-15 20:32:34 +0200

    +§4.5: Recognizing Lexemes

--- sharplox/Lox.Interpreter/Scanner.cs
+++ sharplox/Lox.Interpreter/Scanner.cs
@@ -6,8 +6,8 @@ public class Scanner
 {
   private readonly string _source;
   private readonly ICollection<Token> _tokens = new List<Token>();
-  private long _start = 0;
-  private long _current = 0;
+  private int _start = 0;
+  private int _current = 0;
   private long _line = 1;
 
   public Scanner(string source)
@@ -24,14 +24,46 @@ public class Scanner
     }
 
     _tokens.Add(new(EOF, string.Empty, default, _line));
-
     return _tokens;
 
-    bool IsAtEnd() => _current >= _source.Length;
+    bool IsAtEnd()
+    {
+      return _current >= _source.Length;
+    }
   }
 
   private void ScanToken()
   {
-    throw new NotImplementedException();
+    char character = Advance();
+    switch (character)
+    {
+      case '(': AddToken(LEFT_PAREN); break;
+      case ')': AddToken(RIGHT_PAREN); break;
+      case '{': AddToken(LEFT_BRACE); break;
+      case '}': AddToken(RIGHT_BRACE); break;
+      case ',': AddToken(COMMA); break;
+      case '.': AddToken(DOT); break;
+      case '-': AddToken(MINUS); break;
+      case '+': AddToken(PLUS); break;
+      case ';': AddToken(SEMICOLON); break;
+      case '*': AddToken(STAR); break;
+    }
+
+    char Advance()
+    {
+      return _source[_current++];
+    }
+
+    void AddToken(TokenType type)
+    {
+      this.AddToken(type, default);
+    }
+  }
+
+  private void AddToken(TokenType type, object? literal)
+  {
+    int length = _current - _start;
+    string text = _source.Substring(_start, length);
+    _tokens.Add(new(type, text, literal, _line));
   }
 }
